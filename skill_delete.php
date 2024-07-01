8a53c7734d0fb473cf93ef1ee9b6d9d6f8fd9008
DanielDD73 <136918315+DanielDD73@users.noreply.github.com>
2024-07-01 07:22:55 +0700

    Delete skill_delete.php

--- skill_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once "db.php"; 
-$id = base64_decode($_GET['id']);
-$dbcon->query("DELETE FROM skill WHERE id=$id");
-$_SESSION['skill_delete'] = "Delete Successfully!";
-header('location: skill.php');
-
-?>
